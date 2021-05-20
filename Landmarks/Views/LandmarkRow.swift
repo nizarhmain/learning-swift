9def6952ff8ed8ae6d6da71c1d06709c1dd097f4
nizarhmain <hmain.nizar@gmail.com>
2021-05-20 13:36:53 +0200

    rendering a VStack List

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,34 @@
+//
+//  RowView.swift
+//  Landmarks
+//
+//  Created by nizapizza on 20/05/21.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+    var body: some View {
+        HStack{
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }.previewLayout(.fixed(width: 300, height: 70))
+    }
+}
